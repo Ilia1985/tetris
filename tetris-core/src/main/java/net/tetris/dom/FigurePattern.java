c3748bc4f99172b0693537488ff533b897128e63
ILia Sh <giflmg@mail.ru>
2018-04-23 11:28:30 +0300

    Change Figure

--- tetris-core/src/main/java/net/tetris/dom/FigurePattern.java
+++ tetris-core/src/main/java/net/tetris/dom/FigurePattern.java
@@ -1,10 +1,10 @@
 package net.tetris.dom;
-
+package net.tetris.dom;
 public class FigurePattern {
     private String[] rows;
     private int centerX;
-    private int centerY;
     private int nullzero;
+	private int centerX2;
 
     public FigurePattern(int centerX, int centerY, String... rows) {
         this.rows = rows;
